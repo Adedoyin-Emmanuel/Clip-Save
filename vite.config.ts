4bdee3b256538655510cd320f6ffae63a93ef076
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-05 10:21:06 +0100

    [Updates, Modifications] - Updated the vite.config.ts to fix the 404 route error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    "port": 3000
+  server: {
+    port: 3000,
+    host: 'localhost'
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      plugins: [
+        // Add any Rollup plugins here
+      ],
+    },
+    minify: true,
+    sourcemap: false,
   }
-})
+});
+
+
